fd17cf4c3f59bfa09cd80ea488dfe7a5bce11543
NeoTryn <114293865+NeoTryn@users.noreply.github.com>
2024-04-24 11:33:30 +0200

    everything works!

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var db = require('./db');
+var cors = require('cors');
+
 var app = express();
+
+app.use(cors());
 
 var UserController = require('./User/UserController');
 app.use('/users', UserController);
--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var app = require('./app');
 
 const uri = "mongodb+srv://NeoTryn:Nggyu*99*@cluster0.rezabkh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
