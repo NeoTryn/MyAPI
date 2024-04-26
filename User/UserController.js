dcc6ad844368ca62c2c921ba5a8cf55d19dcf3e9
NeoTryn <eidlerfelix@gmail.com>
2024-04-26 19:26:46 +0200

    we got everything worked out fr fr

--- User/UserController.js
+++ User/UserController.js
@@ -25,37 +25,41 @@ router.get('/', function(req, res) {
     User.find({}).then((users) => {
         res.status(200).send(users);
     }).catch((error) => {
-        return res.status(500).send("That aint work " + error);
+        return res.status(500).send("Problem getting users: " + error);
     });
 });
 
-router.get('/:id', function (req, res) {
+router.get('/id/:id', function (req, res) {
     User.findById(req.params.id).then((user) => {
         if (!user) return res.status(404).send("User not found");
         res.status(200).send(user);
     }).catch((error) => {
-        return res.status(500).send("Problem finding user");
+        return res.status(500).send("Problem finding user: " + error);
     });
 });
 
-router.delete('/:name', function (req, res) {
-    /*User.findByIdAndDelete(req.params.id)
-    .then((user) => {
-        res.status(200).send("User " + user.name + " was deleted.");
-    })
-    .catch((error) => {
-        return res.status(500).send("Problem deleting user");
-    });*/
+router.delete('/name/:name', function (req, res) {
     User.findOneAndDelete({ name : req.params.name})
     .then((user) => {
-        res.status(200).send("User " + user.name + " was deleted.");
+        if(!user) return res.status(404).send("User not found");
+        res.status(200).send(JSON.stringify(user.name));
     })
     .catch((error) => {
         return res.status(500).send("Problem deleting user: " + error);
     });
 });
 
-router.put('/:id', function(req, res) {
+router.delete('/id/:id', function (req, res) {
+    User.findByIdAndDelete(req.params.id).then((user) => {
+        if (!user) return res.status(404).send("User not found");
+        res.status(200).send(JSON.stringify(user.name));
+    })
+    .catch((error) => {
+        return res.status(500).send("Problem deleting user: " + error);
+    });
+});
+
+router.put('/id/:id', function(req, res) {
     User.findByIdAndUpdate(req.params.id, req.body, { new : true })
     .then((user) => {
         if (!user) return res.status(404).send("User not found");
